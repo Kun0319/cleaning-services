cb8d944c6ccb8a749c0c80b293fd8f0cfedc99db
Hong <spdog456@gmail.com>
2023-08-21 19:32:59 +0800

    arrange dirs update(have layout bugs)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Title from "./components/homepage/Title";
 import Process from "./components/homepage/Process";
 import StaffInfos from "./components/homepage/StaffInfos";
-import Narbar from "./components/homepage/narbar";
+import Narbar from "./components/narbar";
 import Indexbg from "./components/homepage/indexbg";
 
 function App() {
