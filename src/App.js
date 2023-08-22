87dfc429db28ceabc8ba469efe84877998be028a
wayne <wayne362066@kimo.com>
2023-08-22 15:33:58 +0800

    fix service

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Process from "./components/homepage/Process";
 import StaffInfos from "./components/homepage/StaffInfos";
 import Narbar from "./components/narbar";
 import Indexbg from "./components/homepage/indexbg";
+import Service from "./components/homepage/Service.js";
 
 function App() {
   return (
     <div className="service-process">
       <Narbar />
       <Indexbg />
+      <Service/>
       <Title />
       <Process />
       <StaffInfos />
