bdf7641a535d3cb05b2cc2bba6844803d57f4b78
cgchung1227 <cg19931227@Gmail.com>
2023-08-24 22:52:11 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import Title from "./components/homepage/Title";
+import Process from "./components/homepage/Process";
+import StaffInfos from "./components/homepage/StaffInfos";
 import Narbar from "./components/narbar";
 import Indexbg from "./components/homepage/indexbg";
 import Service from "./components/homepage/Service.js";
@@ -16,11 +19,6 @@ function App() {
       <StaffInfos />
       <Topic />
       <Footer />
-<<<<<<< HEAD
-=======
-      <Case />
-
->>>>>>> cg
     </div>
   );
 }
