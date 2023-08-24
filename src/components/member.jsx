7286c279c28d1c72212b470af0dad7d1e89fa3dd
Kun <asd1234567896138@gmail.com>
2023-08-24 20:18:27 +0800

    not yet finished

--- src/components/member.jsx
+++ src/components/member.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import Sidebar from "./Sidebar";
 
+
+
 class member extends Component {
-    state = {}
+    state = {};
+
+
+    btn = {
+        height: "2em",
+        width: "8em",
+        color: "#664D03",
+    }
+
+
+
     render() {
         return (
             <div>
@@ -26,10 +38,10 @@ class member extends Component {
 
                     {/* 內容 */}
                     <div className=' ms-3 d-flex row align-items-center justify-content-center'
-                        style={{ width: "1091px", border: "1px solid #ffc107", borderRadius: "50px" }}>
-                        <h3 className='align-items-center justify-content-center'>管理訂單</h3>
-                        <div className="d-flex  align-items-center " style={{ width: "913px", height: "584px", lineHeight: "50px", backgroundColor: "#FFF8E1" }}>
-                            <h5 style={{ color: "#664D03" }}>
+                        style={{ width: "1091px", height: "807px", border: "1px solid #ffc107", borderRadius: "50px" }}>
+                        <h3 className=' justify-content-center'>管理訂單</h3>
+                        <div className="d-flex  align-items-center " style={{ width: "913px", height: "584px", backgroundColor: "#FFF8E1" }}>
+                            <h5 style={{ color: "#664D03", lineHeight: "50px" }}>
                                 訂單編號：C00019<br />
                                 顧客姓名：鍾秋節<br />
                                 電話：0912345667<br />
@@ -39,11 +51,11 @@ class member extends Component {
                                 附註：客廳的花瓶不要移動，那花瓶要十二萬，清理時要小心點。<br />
                             </h5>
                         </div>
-
-                        <div className='d-flex row'>
-                            <button>取消訂單</button>
-                            <button>確認付款</button>
-                            <img src="./images/Frametree.png" alt="" />
+                        {/* 用position? */}
+                        <div className='d-flex row justify-content-center m-5  ' >
+                            <button style={this.btn}>取消訂單</button>
+                            <button style={this.btn}>確認付款</button>
+                            <img src="/images/Frametree.png" alt="" />
                         </div>
                     </div>
                 </div>
