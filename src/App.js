9796de823524cc3aad2c40fb17e1c2bef02caba0
cgchung1227 <cg19931227@Gmail.com>
2023-08-25 10:34:19 +0800

    fixcss

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import Indexbg from "./components/homepage/indexbg";
 import Service from "./components/homepage/Service.js";
 import Topic from "./components/homepage/Topic";
 import Footer from "./components/Footer";
-import Case from "./components/caseshare/case";
+
 
 function App() {
-  return (
-    <div className="service-process">
+  return ( <div>
       <Narbar />
       <Indexbg />
       <Service />
       <Title />
       <Process />
       <StaffInfos />
-      <Topic />
+      <Topic /> 
       <Footer />
-      <Case />
+
 
     </div>
   );
