7a71b8175ae47b04a62a8663f1baf391672bced9
wayne <wayne362066@kimo.com>
2023-08-28 14:49:16 +0800

    wayne

--- src/components/dashboard/OrderDone.jsx
+++ src/components/dashboard/OrderDone.jsx
@@ -1,8 +1,8 @@
-import "./order.css";
-import OrderStaff from "./orderStaff";
+import React from 'react';
+import './order.css';
+import OrderStaff from './orderStaff';
 
 const OrderDone = () => {
-  // 假資料props
   const order = [
     {
       doneTime: (
@@ -11,64 +11,79 @@ const OrderDone = () => {
           <br />
           17:20:13
         </>
-      ), orderStaff: "李大媽", content: "客廳清潔", orderStatus: "完成訂單"
-    }
+      ),
+      orderStaff: '李大媽',
+      content: '客廳清潔',
+      orderStatus: '完成訂單',
+    },
   ];
+
   return (
     <div className="dashOrder">
       <div className="orderTable">
         <div className="orderInfo">
-          <span class="orderCard">
+          <span className="orderCard">
             <img src="images/order.png" alt="" />
-            <p class="orderText ">訂單成立</p>
+            <p className="orderText">訂單成立</p>
           </span>
-          <p class="line"></p>
-          <span class="orderCard">
+          <p className="line"></p>
+          <span className="orderCard">
             <img src="images/orderGoing.png" alt="" />
-            <p class="orderText ">已送達</p>
+            <p className="orderText">已送達</p>
           </span>
-          <p class="line"></p>
-          <span class="orderCard">
+          <p className="line"></p>
+          <span className="orderCard">
             <img src="images/orderCleaning.png" alt="" />
-            <p class="orderText ">清掃中</p>
+            <p className="orderText">清掃中</p>
           </span>
-          <p class="line"></p>
-          <span class="orderCard">
+          <p className="line"></p>
+          <span className="orderCard">
             <img src="images/orderCleaned.png" alt="" />
-            <p class="orderText ">完成清潔</p>
+            <p className="orderText">完成清潔</p>
           </span>
-          <p class="line"></p>
-          <span class="orderCard">
+          <p className="line"></p>
+          <span className="orderCard">
             <img src="images/orderDone.png" alt="" />
-            <p class="orderText ">完成訂單</p>
+            <p className="orderText">完成訂單</p>
           </span>
         </div>
-        <div className="orderThead">
-          <span>訂單成立</span>
-          <span>派遣專員</span>
-          <span>完成清潔</span>
-          <span>完成狀態</span>
-        </div>
-        {order.map((order) => {
-          return (
-            <div className="orderTbody row orderDn">
-              <span className="col">{order.doneTime}</span>
-              <span className="col">{order.orderStaff}</span>
-              <span className="col">{order.content}</span>
-              <span className="col"><button className="orderDnbtn" onClick={() => {
-                alert("AAAAAAAAAAAAAAAAAA");
-              }}>{order.orderStatus}</button></span>
-            </div>
-          );
-        })}
+        <table className="orderTable">
+          <thead>
+            <tr>
+              <th>訂單成立</th>
+              <th>派遣專員</th>
+              <th>完成清潔</th>
+              <th>完成狀態</th>
+            </tr>
+          </thead>
+          <tbody>
+            {order.map((order) => (
+              <tr className="orderTbody row orderDn">
+                <td className="col">{order.doneTime}</td>
+                <td className="col">{order.orderStaff}</td>
+                <td className="col">{order.content}</td>
+                <td className="col">
+                  <button
+                    className="orderDnbtn"
+                    onClick={() => {
+                      alert('AAAAAAAAAAAAAAAAAA');
+                    }}
+                  >
+                    {order.orderStatus}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       <div className="staffInfo">
-      <OrderStaff />
+        <OrderStaff />
       </div>
       <div className="orderBtns">
         <button
           onClick={() => {
-            alert("AAAAAAAAAAAAAAAAAA");
+            alert('AAAAAAAAAAAAAAAAAA');
           }}
           className="orderBtn"
         >
@@ -76,7 +91,7 @@ const OrderDone = () => {
         </button>
         <button
           onClick={() => {
-            alert("AAAAAAAAAAAAAAAAAA");
+            alert('AAAAAAAAAAAAAAAAAA');
           }}
           className="orderBtn"
         >
@@ -86,4 +101,5 @@ const OrderDone = () => {
     </div>
   );
 };
+
 export default OrderDone;
