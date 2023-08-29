4f5e4d8e8cb7ea6745799299b45dbf94a761c6ad
Lin Jheng Bing <leoin920727@gmail.com>
2023-08-29 09:59:41 +0800

    fix

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Narbar from "../components/narbarIcon";
+import Narbar from "../components/navbarIcon";
 import { Outlet } from "react-router-dom";
 
 export default function Book() {
