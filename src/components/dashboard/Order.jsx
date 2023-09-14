194dbb01efa6ca23bb2b89935b17e3600cb1a979
wayne <wayne362066@kimo.com>
2023-09-14 16:41:48 +0800

    0914 會員專區訂單資料抓取

--- src/components/dashboard/Order.jsx
+++ src/components/dashboard/Order.jsx
@@ -1,13 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./order.css";
-import data from "./orderFake.json";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Order = () => {
-  const limitCount = 2;
+  const limitCount = 4;
   const [number, setNumber] = useState(limitCount);
   const [start, setStart] = useState(0); //從哪開始
   const navigate = useNavigate();
+  const [orderAPI, setOrderAPI] = useState([]); //API變數
+
+  // 會員資料API
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await axios.get(
+          "http://localhost:4107/member", {
+          withCredentials: true
+        });
+        if (Array.isArray(result.data)) {
+          setOrderAPI(result.data);
+        } else {
+          console.error("Received non-array data from the API.");
+        }
+        setOrderAPI(() => {
+          return result.data;
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  // 判斷訂單狀態
+  const handleOrderStatus = (state) => {
+    if (state === 0) {
+      return "新訂單";
+    } else if (state === 1) {
+      return "未完成";
+    }
+    return "已完成";
+  };
 
   // 換頁
   const prevPageClick = () => {
@@ -51,42 +85,51 @@ const Order = () => {
         <thead className="orderThead">
           <tr>
             <th>訂單編號</th>
-            <th>訂單內容</th>
+            <th>員工編號</th>
+            <th>訂單日期</th>
+            <th>清潔週數</th>
+            <th>剩餘次數</th>
+            <th>訂單金額</th>
             <th>訂單狀態</th>
           </tr>
         </thead>
         <tbody className="orderTbody">
-          {data
+          {orderAPI
             .slice(start, number)
-            .map(({ index, orderNumber, content, orderStatus }) => {
-              return (
-                <tr
-                  key={index}
-                  onClick={() => {
-                    navigate(`/member/${orderNumber}`);
-                  }}
-                >
-                  <td>{orderNumber}</td>
-                  <td>{content}</td>
-                  <td>
-                    {orderStatus === "0"
-                      ? "新訂單"
-                      : orderStatus === "1"
-                      ? "處理中"
-                      : orderStatus === "99"
-                      ? "已完成"
-                      : "未知狀態"}
-                  </td>
-                </tr>
-              );
-            })}
+            .map(
+              ({
+                ornumber,
+                employeeid,
+                ordertime,
+                weeks,
+                donetime,
+                money,
+                state,
+              }) => {
+                return (
+                  <tr
+                    key={ornumber}
+                    onClick={() => {
+                      navigate(`/member/${ornumber}`);
+                    }}
+                  >
+                    <td>{ornumber}</td>
+                    <td>{employeeid}</td>
+                    <td>{new Date(ordertime).toLocaleDateString('en-CA')}</td>
+                    <td>{weeks}週</td>
+                    <td>{donetime}次</td>
+                    <td>{money}</td>
+                    <td>{handleOrderStatus(state)}</td>
+                  </tr>
+                );
+              })}
         </tbody>
       </table>
       <div className="orderBtn-group">
         <button onClick={() => prevPageClick()} className="orderBtn">
           上一頁
         </button>
-        <button onClick={() => nextPageClick(data)} className="orderBtn">
+        <button onClick={() => nextPageClick(orderAPI)} className="orderBtn">
           下一頁
         </button>
       </div>
