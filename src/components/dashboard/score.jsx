c4ee32983098e4b2aa6ee6bbf3dde479916b84cb
Lin Jheng Bing <leoin920727@gmail.com>
2023-09-24 17:07:14 +0800

    add clearImg

--- src/components/dashboard/score.jsx
+++ src/components/dashboard/score.jsx
@@ -3,48 +3,53 @@ import { useParams } from "react-router-dom";
 import "./order.css";
 import Fraction from "./Fraction";
 import axios from "axios";
+let data = [
+  { id: 1, name: "打掃分數：", value: 5 },
+  { id: 2, name: "效率分數：", value: 5 },
+  { id: 3, name: "態度分數：", value: 5 },
+  { id: 4, name: "細心分數：", value: 5 },
+];
 
-const Score = ({ setModal, orderAPI, staffAPI, evaluateAPI, setUpdataScore}) => {
-  const [counters, setCounters] = useState([]);
+const Score = ({
+  setModal,
+  orderAPI,
+  staffAPI,
+  evaluateAPI,
+  setUpdataScore,
+}) => {
+  const [counters, setCounters] = useState(data);
   const [comment, setComment] = useState("無評論內容");
   const { orderNumber } = useParams();
-  
+
   useEffect(() => {
-    const data = [
-      { id: 1, name: "打掃分數：", value: 5 },
-      { id: 2, name: "效率分數：", value: 5 },
-      { id: 3, name: "態度分數：", value: 5 },
-      { id: 4, name: "細心分數：", value: 5 },
-    ];
-    setCounters(() => {
-      return data;
-    });
-    !comment && setComment("沒有評論")
-  }, [comment]);
-  
+    !comment && setComment("沒有評論");
+  }, [comment, counters]);
+
   async function handleScoreUpdata() {
-    try{
-      await axios.put(
-        `http://localhost:4107/member/updata/${orderNumber}`,
-        { data: counters, comment: comment,orderAPI:orderAPI }
-      );
+    try {
+      await axios.put(`http://localhost:4107/member/updata/${orderNumber}`, {
+        data: counters,
+        comment: comment,
+        orderAPI: orderAPI,
+      });
       setModal(false);
-      setUpdataScore(true)
+      setUpdataScore(true);
       setTimeout(() => {
-      setUpdataScore(false)
-      window.scrollTo(0, 0)
-      window.location.reload()
+        setUpdataScore(false);
+        window.scrollTo(0, 0);
+        window.location.reload();
       }, 2500);
     } catch (error) {
       console.error("Error updata data:", error);
     }
   }
 
-  const staffStar = evaluateAPI ? Object.values(evaluateAPI).reduce((aa, bb) => {
-    aa += bb / 4
-    return aa
-  }, 0) : 0;
-  
+  const staffStar = evaluateAPI
+    ? Object.values(evaluateAPI).reduce((aa, bb) => {
+        aa += bb / 4;
+        return aa;
+      }, 0)
+    : 0;
 
   return (
     <dialog open className="scoreOP">
@@ -53,19 +58,18 @@ const Score = ({ setModal, orderAPI, staffAPI, evaluateAPI, setUpdataScore}) => 
         <img className="orderStaff" src={staffAPI.photo} alt="" />
       </div>
       <div className="scoreBody scoretext">
-        <span>{staffAPI.employeeid}-{staffAPI.employeename}</span>
+        <span>
+          {staffAPI.employeeid}-{staffAPI.employeename}
+        </span>
       </div>
       <div className="scoreStart">
-        <div className="scoreStart" style={{position:"relative"}}>
-        <img
+        <div className="scoreStart" style={{ position: "relative" }}>
+          <img
             src="/images/staffInfo-star.png"
             alt="star-y"
-            style={{ clipPath: `inset(0 ${100 - staffStar * 20}% 0 0)`}}
+            style={{ clipPath: `inset(0 ${100 - staffStar * 20}% 0 0)` }}
           />
-        <img
-          src="/images/staffInfo-star2.png"
-            alt="star-g"
-            />
+          <img src="/images/staffInfo-star2.png" alt="star-g" />
         </div>
       </div>
       <div className="scoreBody scoretext">
